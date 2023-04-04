ec6c43a76c707e7f46d593b4f69385fffec6ef0f
IanB98 <iann.bethel@gmail.com>
2023-04-05 00:09:34 +0100

    Finished secure app

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users
+
+  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 end
